1502a7a4c415aaccbbf5fd50ea2fe92997ee918a
Winfred Gyebi <winfredgyebi@gmail.com>
2025-11-14 13:36:06 -0500

    more work was done on sections

--- phanerosis.js
+++ phanerosis.js
@@ -15,14 +15,54 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
+  
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+  
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function autoSlides() {
+  slideIndex++;              // Move to next slide
+  showSlides(slideIndex);    // Show updated slide
+  setTimeout(autoSlides, 5000); // Change slide every 5 seconds
+}
+
+// Start the automatic slideshow
+autoSlides();
+
+/*
+//modal js
+
+function openModal() {
+  let modal = document.getElementById("imageModal");
+  let largeImage = document.getElementById("largeImage");
+
+  // get the image inside the clicked card
+  let smallImg = document.querySelector("img.pic");
+
+  largeImage.src = smallImg.src;   // use the same picture in large view
+  modal.style.display = "flex";    // show modal
+}
+
+function closeModal() {
+  document.getElementById("imageModal").style.display = "none";
+}
+
+// Close modal by clicking anywhere outside the image
+window.onclick = function (event) {
+  let modal = document.getElementById("imageModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+*/
+
